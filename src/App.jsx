4897c513af2c1b6426dbd7ea48dfc0827b0f94fe
luAr26101 <raul.cosmin.26@icloud.com>
2026-05-13 20:53:54 +0300

    Complete version 1.0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import Values from "values.js";
 import ColorList from "./components/color-list";
 import Form from "./components/form";
-import Values from "values.js";
 
 const App = () => {
   const [colors, setColors] = useState(new Values("#f15025").all(10));
+
+  const addColor = (color) => {
+    try {
+      const newColors = new Values(color).all(10);
+      setColors(newColors);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   return (
     <main>
-      <Form />
+      <Form addColor={addColor} />
       <ColorList colors={colors} />
+      <ToastContainer position='top-center' />
     </main>
   );
 };
--- src/components/single-color.jsx
+++ src/components/single-color.jsx
@@ -1,8 +1,23 @@
+import { toast } from "react-toastify";
+
 function SingleColor({ color, index }) {
   const { hex, weight } = color;
   console.log(color);
+
+  async function saveToClipboard() {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success(`Copied #${hex} to clipboard!`);
+      } catch (err) {
+        toast.error("Clipboard access not available.");
+      }
+    }
+  }
+
   return (
     <article
+      onClick={saveToClipboard}
       className={index > 10 ? "color color-light" : "color"}
       style={{ backgroundColor: `#${hex}` }}
     >
