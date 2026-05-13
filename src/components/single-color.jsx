05ce24b59684f836b0357e05f21f11784e4ef55c
luAr26101 <raul.cosmin.26@icloud.com>
2026-05-13 20:09:33 +0300

    Add the color palette

--- src/components/single-color.jsx
+++ src/components/single-color.jsx
@@ -1,6 +1,15 @@
 function SingleColor({ color, index }) {
+  const { hex, weight } = color;
   console.log(color);
-  return <>Single color</>;
+  return (
+    <article
+      className={index > 10 ? "color color-light" : "color"}
+      style={{ backgroundColor: `#${hex}` }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>#{hex}</p>
+    </article>
+  );
 }
 
 export default SingleColor;
